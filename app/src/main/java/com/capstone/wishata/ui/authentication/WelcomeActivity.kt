fbbbe13be3ebb22f1147ae1a549023df878e5b89
Rahdriawan Prismana <prismanarahdriawan@gmail.com>
2024-06-14 01:17:48 +0700

    home: Refresh Theme

--- app/src/main/java/com/capstone/wishata/ui/authentication/WelcomeActivity.kt
+++ app/src/main/java/com/capstone/wishata/ui/authentication/WelcomeActivity.kt
@@ -2,10 +2,7 @@ package com.capstone.wishata.ui.authentication
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.capstone.wishata.R
 import com.capstone.wishata.databinding.ActivityWelcomeBinding
 
--- app/src/main/java/com/capstone/wishata/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/wishata/ui/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -38,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        supportActionBar
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_wishlist, R.id.navigation_profile
