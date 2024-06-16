e1f56f3e2963be03efff3cdee5cd8b7c2e06741b
Rahdriawan Prismana <prismanarahdriawan@gmail.com>
2024-06-16 21:58:43 +0700

    home: Fix Splash Screen

--- app/src/main/java/com/capstone/wishata/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/capstone/wishata/ui/splash/SplashActivity.kt
@@ -1,11 +1,14 @@
 package com.capstone.wishata.ui.splash
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.capstone.wishata.R
+import com.capstone.wishata.ui.main.MainActivity
+import java.lang.Exception
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,21 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val thread = Thread{
+
+            run {
+
+                try {
+                    Thread.sleep(1500)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+            }
+        }
+
+        thread.start()
     }
 }
